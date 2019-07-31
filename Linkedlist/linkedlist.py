13fa93e05eb8d959d935aa3a5c9d2044b7e9744b
Sreenath S <sreenathsirobhushanam@gmail.com>
2019-08-01 00:38:29 +0530

    Linked list insertion

--- Linkedlist/linkedlist.py
+++ Linkedlist/linkedlist.py
@@ -33,10 +33,11 @@ class LinkedList():
             self.head = new_node
             self.length += 1
         else:
-            new_node.set_next(self.head)
-            self.head = new_node
+            current = self.head
+            while current.has_next():
+                current = current.get_next()
+            current.set_next(new_node)
             self.length += 1
-        return True
 
     def find_nth_element(self, n):
         count = 0
@@ -51,12 +52,15 @@ class LinkedList():
     def printNode(self):
         current = self.head
         while current:
-            print(current.data)
+            print(current.data, end=' ')
             current = current.get_next()
 
     def find_middle_element(self):
         count = 0
-        mid_pos = self.length / 2
+        mid_pos = int(self.length / 2)
+        if self.length == 0:
+            print('list is empty. So no middle element')
+            return
         if self.length % 2 == 0:
             print('mid elements are at position {} and {}'.format(
                 int(mid_pos), int(mid_pos + 1)))
@@ -71,26 +75,27 @@ class LinkedList():
             print('mid element is at position {}'.format(mid_pos))
             current = self.head
             while current:
-                count += 1
+                # count += 1
                 if mid_pos == count:
                     print('element at position {} is {}'.format(
                         mid_pos, current.data))
                 current = current.get_next()
+                count += 1
 
 
 mylist = LinkedList()
-print('inserting in linked list')
-print(mylist.insert(1))
-print(mylist.insert(2))
-print(mylist.insert(3))
-print(mylist.insert(8))
-print(mylist.insert(10))
-print(mylist.insert(11))
-print(mylist.insert(12))
-print(mylist.insert(13))
-print(mylist.insert(14))
-print(mylist.insert(15))
-print('Elements of linked list')
+# print('inserting in linked list')
+mylist.insert(1)
+mylist.insert(2)
+mylist.insert(3)
+mylist.insert(8)
+mylist.insert(10)
+mylist.insert(11)
+mylist.insert(12)
+mylist.insert(13)
+mylist.insert(14)
+mylist.insert(15)
+# print('Elements of linked list')
 mylist.printNode()
 # mylist.find_nth_element(3)
-mylist.find_middle_element()
+# mylist.find_middle_element()
