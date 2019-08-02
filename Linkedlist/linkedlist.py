1db6c0965c5d621822304ebb38c9d5baf5bd5820
Sreenath S <sreenathsirobhushanam@gmail.com>
2019-08-02 11:09:15 +0530

    Linked list ADT

--- Linkedlist/linkedlist.py
+++ Linkedlist/linkedlist.py
@@ -25,6 +25,7 @@ class LinkedList():
         self.head = None
         self.length = 0
 
+    # The below function will add the elements at last in the linked list.
     def insert(self, data):
         new_node = Node()
         new_node.data = data
@@ -51,8 +52,9 @@ class LinkedList():
 
     def printNode(self):
         current = self.head
+        print('Elements in the linked list')
         while current:
-            print(current.data, end=' ')
+            print(current.data)
             current = current.get_next()
 
     def find_middle_element(self):
@@ -82,20 +84,86 @@ class LinkedList():
                 current = current.get_next()
                 count += 1
 
+    def insert_at_beginning(self, data):
+        new_node = Node()
+        new_node.set_data(data)
+        if self.length == 0:
+            self.head = new_node
+            self.length += 1
+        else:
+            current = self.head
+            new_node.set_next(current)
+            self.head = new_node
+            self.length += 1
+
+    def insert_at_middle(self, pos, data):
+        if pos > self.length or pos < 0:
+            return None
+        else:
+            if pos == 0:
+                self.insert_at_beginning(data)
+            else:
+                current = self.head
+                new_node = Node()
+                new_node.set_data(data)
+                count = 0
+                while count < pos - 1:
+                    count += 1
+                    current = current.get_next()
+                new_node.set_next(current.get_next())
+                current.set_next(new_node)
+                self.length += 1
+
+    def insert_at_end(self, data):
+        current = self.head
+        new_node = Node()
+        new_node.set_data(data)
+        while current.has_next():
+            current = current.get_next()
+        current.set_next(new_node)
+        new_node.set_next(None)
+        self.length += 1
+
+    def delete_at_beginning(self):
+        if self.length == 0:
+            print('List is empty')
+        else:
+            current = self.head
+            self.head = current.get_next()
+            current.set_next(None)
+            self.length -= 1
+
+    def delete_at_middle(self, data):
+        current = self.head
+        previous = self.head
+        while current:
+            if current.data == data:
+                previous.set_next(current.get_next())
+                current.set_next(None)
+                self.length -= 1
+                return
+            previous = current
+            current = current.get_next()
+
+    def delete_at_end(self):
+        current = self.head
+        previous = current
+        while current.has_next():
+            previous = current
+            current = current.get_next()
+        previous.set_next(None)
+        self.length -= 1
+
 
 mylist = LinkedList()
-print('inserting in linked list')
-mylist.insert(1)
 mylist.insert(2)
 mylist.insert(3)
-mylist.insert(8)
-mylist.insert(10)
-mylist.insert(11)
-mylist.insert(12)
-mylist.insert(13)
-mylist.insert(14)
-mylist.insert(15)
-print('Elements of linked list')
+mylist.insert(5)
 mylist.printNode()
-mylist.find_nth_element(3)
-mylist.find_middle_element()
+mylist.insert_at_beginning(1)
+mylist.insert_at_middle(3, 4)
+mylist.insert_at_end(6)
+mylist.delete_at_beginning()
+mylist.delete_at_middle(4)
+mylist.delete_at_end()
+mylist.printNode()
